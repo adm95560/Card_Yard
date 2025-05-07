4cd5f6b36fbc0b03e4f4f860d0d5485e066597e2
adm95560 <168374903+adm95560@users.noreply.github.com>
2025-05-07 19:19:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
+
+#define LIGNES 3
+#define COLONNES 4
+#define MAX_JOUEURS 8
+#define NB_MAX_CARTES 150
+
+// Codes couleur ANSI
+#define RESET       "\033[0m"
+#define VIOLET      "\033[35m"
+#define BLEU_CLAIR  "\033[36m"
+#define VERT        "\033[32m"
+#define JAUNE       "\033[33m"
+#define ROUGE       "\033[31m"
+
 #include "jeu.h"
-#include "affichage.c"
+#include "affichage.h"
+
 
 int main() {
     srand((unsigned)time(NULL));
@@ -17,10 +33,11 @@ int main() {
         partie.joueur_actuel = (partie.joueur_actuel + 1) % partie.nb_joueurs;
     }
 
-    printf("\n✅ La partie est terminée ! Scores :\n");
-    for (int i = 0; i < partie.nb_joueurs; i++) {
-        printf("Joueur %d (%s) : %d points\n", i + 1, partie.joueurs[i].nom, calculer_score(&partie.joueurs[i]));
-    }
+    printf("\n✅ La partie est terminée ! \n Scores :\n");
+for (int i = 0; i < partie.nb_joueurs; i++) {
+    printf("Joueur %d (%s) : %d points\n", i + 1, partie.joueurs[i].nom, calculer_score(&partie.joueurs[i]));
+}
 
+afficher_gagnant(&partie);
     return 0;
 }
